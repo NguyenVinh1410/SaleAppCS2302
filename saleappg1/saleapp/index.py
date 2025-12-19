119cab73a423cdaa9bd1414e29da804f3a98f2ef
nguyenvink1005@gmail.com <nguyenvink1005@gmail.com>
2025-12-19 16:13:23 +0700

    create project

--- saleappg1/saleapp/index.py
+++ saleappg1/saleapp/index.py
@@ -3,15 +3,22 @@ from saleapp import dao
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     q = request.args.get("q")
     cate_id = request.args.get("cate_id")
-    cates = dao.load_categories()
     prods = dao.load_products(q=q, cate_id=cate_id)
-    return render_template("index.html", cates=cates, prods=prods)
+    return render_template("index.html", prods=prods)
 
+@app.route("/products/<int:id>")
+def details(id):
+    prod = dao.get_product_by_id(id)
+    return render_template("product-details.html", prod=prod)
 
+@app.context_processor
+def common_attribute():
+    return{
+        "cates": dao.load_categories()
+    }
 if __name__ == "__main__":
     app.run(debug=True)
